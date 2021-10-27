80de98147ee0410a83fd41844c23de1e5e3efd84
Bryan Huynh <32778577+BryanHuynh@users.noreply.github.com>
2021-10-26 22:56:49 -0600

    added flag that will subtract one from all file names

--- Assets/fix.py
+++ Assets/fix.py
@@ -3,23 +3,33 @@ import sys
 import re
 
 
-def main(directory: str):
+def main(flag: bool, directory: str):
     """
     Fixes the directory structure of the assets folder.
+    :param flag: subtract one from all files
     :param directory: The directory to fix.
     :return: None
     """
+    remove = 0
+    if flag:
+        remove = 1
+
     for root, dirs, files in os.walk(directory):
         files.sort()
         for file in files:
             if file.endswith(".png"):
-                name = int(re.search('(\d*).png', file).group(1)) - 1
+
+                name = int(re.search('(\d*).png', file).group(1)) - remove
 
                 os.rename(os.path.join(root, file), os.path.join(root, str(name) + ".png"))
                 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-       main(sys.argv[1])
+    if len(sys.argv) >= 2:
+        if(sys.argv[1] == "-m"):
+            main(True, sys.argv[2])
+        else:
+            main(False, sys.argv[1])
     else:
-        print("Usage: fix.py <directory>")
+        print("Usage: fix.py [-minus] <directory>")
+        print("-minus: subtract one from the all the file names")
