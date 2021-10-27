b7f0f8bea2a93dc7616810f9f24890f1dcf7584f
AlexanderChao14 <chaoalexander45@gmail.com>
2021-10-27 02:14:35 -0600

    Try connecting the teachable machine

--- script.js
+++ script.js
@@ -1,42 +1,66 @@
-const answer = ["Up", "Up", "Right", "Right","Down", "Down", "Left", "Left"]
+const answer = ["Up", "Up", "Right"]
 let seq = []
 let i = 0;
 let fpsY= 75;
-let fpsX= 100;
+let fpsX= 135;
+let currentlyMoving =false;
 
-document.onkeydown = function (event) {
-    switch (event.keyCode) {
-       case 37:
-          console.log("Left key is pressed.");
-          
-          
-          var myTimer = setInterval(() => Move("Left", 8 , myTimer), fpsX);
-          
-          console.log("Done walking")
-          break;
-       case 38:
-          console.log("Up key is pressed.");
 
-        var myTimer = setInterval(() => Move("Up", 16, myTimer), fpsY);
+
+// document.onkeydown = function (event) {
+
+//     switch (event.keyCode) {
+//         case 37:
+            
+//             moveLeft();
+//             break;
+//         case 38: 
         
+//             moveUp();
+//             break;
+//         case 39:
+        
+//             moveRight();
+//             break;
+//         case 40:
+        
+//             moveDown();
+//             break;
+//         }
 
-          break;
-       case 39:
-          console.log("Right key is pressed.");
+//  };
 
-          var myTimer = setInterval(() => Move("Right",5,myTimer), fpsX);
-        console.log("Done walking")
 
-          break;
-       case 40:
-          console.log("Down key is pressed.");
-
-          var myTimer = setInterval(() => Move("Down",18,myTimer), fpsY);
-        console.log("Done walking")
-
-          break;
+function moveLeft(){  
+    if(!currentlyMoving){
+        currentlyMoving=true;
+        var myTimer = setInterval(() => Move("Left", 8 , myTimer), fpsX);
     }
- };
+}
+
+
+function moveUp(){  
+    if(!currentlyMoving){
+        currentlyMoving=true;
+        var myTimer =  setInterval(() => Move("Up", 16, myTimer), fpsY);
+    }
+}
+
+function moveRight(){  
+    if(!currentlyMoving){
+        currentlyMoving=true;
+        var myTimer = setInterval(() => Move("Right",5,myTimer), fpsX);
+    }
+}
+
+function moveDown(){  
+    if(!currentlyMoving){
+        currentlyMoving=true;
+        var myTimer = setInterval(() => Move("Down",18,myTimer), fpsY);
+    }
+}
+
+
 
 function combination (input){
     
@@ -66,6 +90,95 @@ function Move(direction, max, myTimer){
                 combination(direction) //TODO
                 console.log("Done walking")
                 clearInterval(myTimer);
+                currentlyMoving =false;
               }
              i++
 }
+
+
+
+
+const URL = "https://teachablemachine.withgoogle.com/models/X1SkYaAOO/";
+
+let model, webcam, labelContainer, maxPredictions;
+
+// Load the image model and setup the webcam
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    // Convenience function to setup a webcam
+    const flip = true; // whether to flip the webcam
+    webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+
+    // append elements to the DOM
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+        labelContainer.appendChild(document.createElement("div"));
+    }
+}
+
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+// run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(webcam.canvas);
+    let result ='';
+    let prob = 0;
+    
+    for (let i = 0; i < maxPredictions; i++) {
+        
+        const classPrediction =
+            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+        labelContainer.childNodes[i].innerHTML = classPrediction;
+        // console.log(prediction[i].probability, prob)
+        if((Math.round(prediction[i].probability  * 100) / 100) > (Math.round(prob * 100) / 100)){
+            
+            result = prediction[i].className
+            prob=prediction[i].probability
+            // console.log("------------------------------------",result, prob)
+            // console.log(result + " Loooooook here")
+
+            if("Left" == result){
+                console.log("Left");
+                moveLeft();
+                
+            }else if("Up" == result){
+                console.log("Up");
+                moveUp();
+            }else if("Right" == result){
+                console.log("Right");
+                moveRight();
+            }else if("Down" == result){
+                console.log("Down");
+                moveDown();
+            }
+            else{
+                // console.log("Nothing")
+            }
+            
+        }
+        // console.log("asdfhksahfhsalkdhfshh",prediction[i].probability, prob)
+        
+        
+    }
+    $('#result').text(result);
+}
+
+init();
