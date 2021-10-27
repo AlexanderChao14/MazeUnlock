effd5314b4693a0cc2176bb98559a96a136e12c1
Bryan Huynh <32778577+BryanHuynh@users.noreply.github.com>
2021-10-26 22:38:12 -0600

    added usage to script

--- Assets/fix.py
+++ Assets/fix.py
@@ -19,4 +19,7 @@ def main(directory: str):
                 
 
 if __name__ == '__main__':
-   main(sys.argv[1])
+    if len(sys.argv) == 2:
+       main(sys.argv[1])
+    else:
+        print("Usage: fix.py <directory>")
